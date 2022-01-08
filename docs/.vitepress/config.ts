9fa2bb5cb133e37926e8480a0a0cb711a15650b2
wangkaiwd <1162486176@qq.com>
2022-01-08 19:45:21 +0800

    feat:complete try vitepress

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,15 @@ export default defineConfig({
   title: 'TypeScript Library Template',
   description: 'Quick create a typescript library with zero configuration',
   base: '/vitepress-demo/',
+  themeConfig: {
+    nav: [
+      {
+        link: 'https://github.com/wangkaiwd/vitepress-demo',
+        text: 'github'
+      }
+    ],
+    sidebar: false
+  },
   markdown: {
     lineNumbers: true
   }
