f0da20db74646328be41ad88f9aa56fac5bdc7c0
wangkaiwd <1162486176@qq.com>
2022-01-08 17:30:39 +0800

    feat: first commit

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'TypeScript Library Template',
+  description: 'Quick create a typescript library with zero configuration',
+  base: '/vitepress-demo/',
+  markdown: {
+    lineNumbers: true
+  }
+})
